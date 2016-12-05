bed93b7d75765cebdf2b422e89ab89eaf66b4be3
Administrator <Administrator@7A304-0017>
2016-12-05 17:30:26 +0800

    初始代码

---
+++ src/Fragment/BaseInputCellFragment.java
@@ -0,0 +1,10 @@
+package Fragment;
+
+import android.app.Fragment;
+
+public abstract class BaseInputCellFragment extends Fragment {
+
+	public abstract void setLabelText(String labelText);
+	public abstract void setHintText(String hintText);
+	
+}
